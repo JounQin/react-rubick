0591380645b27272af0ff470382cc4620eaeb007
Jiaren Liu <jrliu@alauda.io>
2018-03-15 00:10:05 +0800

    first blood, show nothing...

---
+++ server/dev.ts
@@ -0,0 +1,7 @@
+import koaWebpack from 'koa-webpack'
+
+import config from '../build/webpack.config'
+
+export default koaWebpack({
+  config,
+})
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { renderRoutes, RouteConfig } from 'react-router-config'
+
+import Landing from './Landing'
+
+const routes: RouteConfig[] = [
+  {
+    path: '',
+    component: Landing,
+  },
+]
+
+const App = () => renderRoutes(routes)
+
+export default App
